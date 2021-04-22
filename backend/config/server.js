de153524209e51fd4229214cc1b7077370f8644e
Amaury Soto <amaurysotog@gmail.com>
2021-04-21 23:21:20 -0500

    Folder config backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9713ff5f1f94b6174d53bd067978875b'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e524f27a-66d9-4c9b-acf0-46738699fc35'
+};
