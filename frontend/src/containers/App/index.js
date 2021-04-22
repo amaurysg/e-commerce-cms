24b9b1c708d5047feb84d88157f7dda524c9b846
Amaury Soto <amaurysotog@gmail.com>
2021-04-22 11:44:39 -0500

    Query Articles, Render Articles Main

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../../components/Nav'
+import Articles from '../Articles'
 
 const App = () => {
   return (
     <div className="App">
       <Nav />
+      <Articles />
     </div>
   )
 }
